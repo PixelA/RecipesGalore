5b1d537fe1c40503134603ce4959682fc1698bdc
Asherlee Levy <asherleelevy@asherlees-macbook-pro.local>
2016-06-07 10:52:16 -0400

    Complete the Create Recipe VC finished working code

--- RecipesGalore/CreateRecipeVC.swift
+++ RecipesGalore/CreateRecipeVC.swift
@@ -7,29 +7,61 @@
 //
 
 import UIKit
+import CoreData
 
-class CreateRecipeVC: UIViewController {
-
+class CreateRecipeVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var recipeTitle: UITextField!
+    
+    @IBOutlet weak var recipeIngerdients: UITextField!
+    
+    @IBOutlet weak var recipeSteps: UITextField!
+    
+    @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var addRecipeBtn: UIButton!
+    
+    var imagePicker: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    imagePicker = UIImagePickerController()
+    imagePicker.delegate = self
+        recipeImage.layer.cornerRadius = 4.0
+        recipeImage.clipsToBounds = true
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        recipeImage.image = image
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func addImage(sender: AnyObject) {
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
-    */
-
+    
+    @IBAction func createRecipe(sender: AnyObject) {
+        if let title = recipeTitle.text where title != "" {
+            
+            let app = UIApplication.sharedApplication().delegate as! AppDelegate
+            let context = app.managedObjectContext
+            let entity = NSEntityDescription.entityForName("Recipe", inManagedObjectContext: context)!
+            let recipe = Recipe(entity: entity, insertIntoManagedObjectContext: context)
+            recipe.title = title
+            recipe.ingredients = recipeIngerdients.text
+            recipe.steps = recipeSteps.text
+            recipe.setRecipeImage(recipeImage.image!)
+            
+            context.insertObject(recipe)
+            
+            do {
+                try context.save()
+            } catch {
+                print("Could not save Recipe")
+            }
+            self.navigationController?.popViewControllerAnimated(true)
+        }
+    }
+    
 }
